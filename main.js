cb88e6cc47043c495eb2710c95ceeee846ebf41e
Yash Bhutoria <Yash.Bhutoria@welkgroup.com>
2020-05-28 16:39:02 +0530

    Initial Upload

---
+++ main.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const convertButton = document.getElementById("convertButton")
+const normalTextBox = document.getElementById("normal-text")
+const ironicTextBox = document.getElementById("ironic-text")
+
+
+function main(){
+    alert("Button Works");
+}
+
+convertButton.addEventListener("click", main);
